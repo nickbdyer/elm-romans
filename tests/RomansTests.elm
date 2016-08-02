a81b4da473e1e8a3b723e39cde5691e029ca3feb
Nick Dyer <nbdyer@gmail.com>
2016-08-02 16:18:56 +0100

    Test formatting changes

--- tests/RomansTests.elm
+++ tests/RomansTests.elm
@@ -5,32 +5,40 @@ import ElmTest exposing (..)
 
 tests =
     suite "Roman Numerals Test"
-        [ 
-          test "Converts 0" 
+        [ test 
+          "Converts 0" 
           <| assertRomans "" 0
 
-        , test "Converts 1" 
+        , test 
+          "Converts 1" 
           <| assertRomans "I" 1
 
-        , test "Converts 5"
+        , test 
+          "Converts 5"
           <| assertRomans "V" 5
 
-        , test "Converts 10"
+        , test 
+          "Converts 10"
           <| assertRomans "X" 10
 
-        , test "Converts 4"
+        , test 
+          "Converts 4"
           <| assertRomans "IV" 4
 
-        , test "Converts 9"
+        , test 
+          "Converts 9"
           <| assertRomans "IX" 9
 
-        , test "Converts 2"
+        , test 
+          "Converts 2"
           <| assertRomans "II" 2
 
-        , test "Converts 2"
+        , test 
+          "Converts 2"
           <| assertRomans "II" 2
 
-        , test "Converts 28"
+        , test 
+          "Converts 28"
           <| assertRomans "XXVIII" 28
         ]
 
@@ -41,4 +49,4 @@ assertRomans roman arabic =
 
 main : Program Never
 main =
-    runSuite tests
+    runSuiteHtml tests
