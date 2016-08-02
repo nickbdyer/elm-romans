5a2f7c4798039554c8b5291a6cf04c421aa7d055
Nick Dyer <nbdyer@gmail.com>
2016-08-02 16:57:12 +0100

    Refactor to one line

--- src/Romans.elm
+++ src/Romans.elm
@@ -5,12 +5,4 @@ import String exposing(repeat)
 
 convert : Int -> String
 convert num =
-  if num == 3 then
-     repeat 3 "I"
-  else if num == 2 then
-     repeat 2 "I"
-     "II"
-  else if num == 1 then
-     repeat 1 "I"
-  else
-    ""
+    repeat num "I"
