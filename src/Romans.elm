a72af4325018c42547f26674827411b23bd4d1c1
Nick Dyer <nbdyer@gmail.com>
2016-08-03 10:30:34 +0100

    Removes if for 6, refactored

--- src/Romans.elm
+++ src/Romans.elm
@@ -10,9 +10,7 @@ convert num =
 
 convertRecursive : Int -> String -> String
 convertRecursive arabic roman =
-  if arabic == 6 then
-    "VI"
-  else if arabic >= 5 then
+  if arabic >= 5 then
     convertRecursive (arabic - 5) (roman ++ "V")
   else
     roman ++ (repeat arabic "I")
