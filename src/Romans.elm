996ad86da72fa00e3046b2e7eead2299b96bd801
Nick Dyer <nbdyer@gmail.com>
2016-08-03 10:28:33 +0100

    Indenting resolved with new elm-vim and added solution for 5 and 6

--- src/Romans.elm
+++ src/Romans.elm
@@ -5,10 +5,14 @@ import String exposing(repeat)
 
 convert : Int -> String
 convert num =
-  if num == 6 then
-     "VI"
-     else if num == 5 then
-     "V"
-     else
-     repeat num "I"
+  convertRecursive num ""
 
+
+convertRecursive : Int -> String -> String
+convertRecursive arabic roman =
+  if arabic == 6 then
+    "VI"
+  else if arabic >= 5 then
+    convertRecursive (arabic - 5) (roman ++ "V")
+  else
+    roman ++ (repeat arabic "I")
--- tests/RomansTests.elm
+++ tests/RomansTests.elm
@@ -4,33 +4,34 @@ import Romans exposing (..)
 import ElmTest exposing (..)
 
 tests =
-    suite "Roman Numerals Test"
-        [ test 
-          "Converts 0" 
-          <| assertEqual "" (Romans.convert 0)
-        
-        , test
-          "Converts 1"
-          <| assertEqual "I" (Romans.convert 1)
+  suite "Roman Numerals Test"
+    [ 
+      test 
+      "Converts 0" 
+      <| assertEqual "" (Romans.convert 0)
 
-        , test
-          "Converts 2"
-          <| assertEqual "II" (Romans.convert 2)
+      , test
+      "Converts 1"
+      <| assertEqual "I" (Romans.convert 1)
 
-        , test
-          "Converts 3"
-          <| assertEqual "III" (Romans.convert 3)
+      , test
+      "Converts 2"
+      <| assertEqual "II" (Romans.convert 2)
 
-        , test
-          "Converts 5"
-          <| assertEqual "V" (Romans.convert 5)
+      , test
+      "Converts 3"
+      <| assertEqual "III" (Romans.convert 3)
 
-        , test
-          "Converts 6"
-          <| assertEqual "VI" (Romans.convert 6)
-        ]
+      , test
+      "Converts 5"
+      <| assertEqual "V" (Romans.convert 5)
+
+      , test
+      "Converts 6"
+      <| assertEqual "VI" (Romans.convert 6)
+    ]
 
 
 main : Program Never
 main =
-    runSuite tests
+  runSuite tests
